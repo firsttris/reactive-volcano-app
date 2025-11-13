6d9e36311e5bc5f6657df3626dc413cf3ea87ebe
Tristan Teufel <tristanteufel@googlemail.com>
2025-11-13 17:53:43 +0100

    redirect to connect page after disconnect

--- src/components/volcano/Workflow/WorkflowWrapper.tsx
+++ src/components/volcano/Workflow/WorkflowWrapper.tsx
@@ -1,9 +1,28 @@
 import { RouteSectionProps } from "@solidjs/router";
 import { VolcanoDeviceProvider } from "../../../provider/VolcanoDeviceProvider";
+import { useBluetooth } from "../../../provider/BluetoothProvider";
+import { useNavigate } from "@solidjs/router";
+import { createEffect } from "solid-js";
+import { ConnectionState } from "../../../utils/uuids";
+import { buildRoute } from "../../../routes";
 
 /**
  * Wrapper component that provides VolcanoDeviceProvider for workflow routes
  */
 export const WorkflowWrapper = (props: RouteSectionProps) => {
+  const navigate = useNavigate();
+  const { connectionState } = useBluetooth();
+
+  // Redirect to connect page if not connected
+  createEffect(() => {
+    const state = connectionState();
+    if (
+      state === ConnectionState.NOT_CONNECTED ||
+      state === ConnectionState.CONNECTION_FAILED
+    ) {
+      navigate(buildRoute.root());
+    }
+  });
+
   return <VolcanoDeviceProvider>{props.children}</VolcanoDeviceProvider>;
 };
